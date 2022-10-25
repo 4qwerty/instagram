6330ac51cf10e90d6c0ee9dcb07b586db63e0871
Artem Yakunenko <artem.yakunenko@plus4u.net>
2022-10-25 17:43:58 +0300

    delete some trash

--- client/src/api/users.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const fetchUser = async () => {
-    const url = 'localhost:8000/get';
-    const response = await axios.get(url);
-    console.log(response.data);
-};
